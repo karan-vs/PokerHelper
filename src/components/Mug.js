8f26b3399f14281ac9301c2c6eebb4f0b56ac41c
Karan Mohan Bhola <>
2018-06-16 15:09:22 +0530

    added mug cards

--- src/components/Mug.js
+++ src/components/Mug.js
@@ -6,6 +6,9 @@ const Mug = ({ deck }) => {
     return (
         <div className="mug">
             <CardSlot cardIndex={-1} deck={deck} />
+            <CardSlot cardIndex={-2} deck={deck} />
+            <CardSlot cardIndex={-3} deck={deck} />
+            <CardSlot cardIndex={-4} deck={deck} />
         </div>
     )
 }
